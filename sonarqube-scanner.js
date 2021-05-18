d11c43e0ac1669388f53894476db149a4d3f3a9b
suvarnataware <33306798+suvarnataware@users.noreply.github.com>
2021-05-18 13:32:01 +0530

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -4,6 +4,8 @@ scanner(
   serverUrl: "http://localhost:9000",
   login:"admin",
   password:"admin",
+  //instead of login password use token from sonarqube
+  //token: "dddsddsdsds"
   options: {
     "sonar.sources": "./src"
   },
